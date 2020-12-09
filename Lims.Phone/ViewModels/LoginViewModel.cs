25b997fc825bc52ba1876463b16b46f2e3095bd3
尹庆文 <Yinqingwen@gmail.com>
2020-12-09 17:40:44 +0800

    20201209  17:40

--- Lims.Phone/ViewModels/LoginViewModel.cs
+++ Lims.Phone/ViewModels/LoginViewModel.cs
@@ -1,36 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace Lims.Phone.ViewModels
 {
     public class LoginViewModel : INotifyPropertyChanged
     {
+        private bool _islogin;
         /// <summary>
         /// 是否登录成功标志位
         /// </summary>
-        public bool IsLogin { get; set; }
+        public bool IsLogin
+        {
+            get { return _islogin; }
+            set
+            {
+                _islogin = value;
+                OnPropertyChanged();
+                //OnPropertyChanged(nameof(Message));
+            }
+        }
 
+        private string _username;
         /// <summary>
         /// 登录用户名
         /// </summary>
-        public string UserName { get; set; }
+        public string UserName 
+        {
+            get { return _username; }
+            set 
+            {
+                _username = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Message));
+            }
+        }
 
+        private string _password;
         /// <summary>
         /// 登录密码
         /// </summary>
-        public string Password { get; set; }
+        public string Password 
+        {
+            get { return _password; }
+            set 
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
 
+        private string _companycode;
         /// <summary>
         /// 公司代码
         /// </summary>
-        public string CompanyCode { get; set; }
+        public string CompanyCode 
+        {
+            get { return _companycode; }
+            set 
+            {
+                _companycode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isautologin = false;
+        public bool IsAutoLogin 
+        {
+            get { return _isautologin; }
+            set 
+            {
+                _isautologin = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+        
+        /// <summary>
+        /// 信息
+        /// </summary>
+        public string Message
+        {
+            get 
+            {
+                var msg = IsAutoLogin ? "下次将会自动登录" : "下次不会自动登录";
+
+                return $"欢迎{UserName}.{msg}";
+            }
+        }
+
+        private bool _isbusy;
+        /// <summary>
+        /// 是否处于忙碌状态
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return _isbusy; }
+            set 
+            {
+                _isbusy = value;
+                OnPropertyChanged();
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public Command LoginCommand { get; }
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(name));
+        }
 
         public LoginViewModel()
         {
-        } 
+        }
     }
 }
--- Lims.Phone/Views/LoginPage.xaml.cs
+++ Lims.Phone/Views/LoginPage.xaml.cs
@@ -3,7 +3,8 @@ using Lims.Phone.Models;
 using Lims.Phone.Services.Identity;
 using Lims.Phone.ViewModels;
 using System;
-
+using System.Collections;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -21,6 +22,7 @@ namespace Lims.Phone.Views
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            
             //数据校验定义调用
             LoginViewModelValidator validationRules = new LoginViewModelValidator();
 
@@ -29,7 +31,6 @@ namespace Lims.Phone.Views
             ValidationResult validationResult = validationRules.Validate(loginViewModel);
             if (validationResult.IsValid)
             {
-                //DisplayAlert("提示信息","界面数据校验成功，准备进行后台校验","确定");
                 AccountInfo accountInfo = Identity.Authenticate(loginViewModel);
                 if (accountInfo.ResulitInfo.IsOK)
                 {
@@ -74,7 +75,7 @@ namespace Lims.Phone.Views
                     //传递参数保存
                     App.Current.SavePropertiesAsync();
                     //返回到主页面
-                    App.Current.MainPage.Navigation.PopAsync(true);// .PushAsync(new MainPage());
+                    App.Current.MainPage.Navigation.PopAsync(true);
                 }
                 else
                 {
@@ -84,29 +85,59 @@ namespace Lims.Phone.Views
             }
             else
             {
+                DisplayError(validationResult.Errors);
                 //界面校验失败，显示错误信息
                 //定义临时参数
-                string message = string.Empty;
-                string controllname = string.Empty;
-                int i = 0;
+                //string message = string.Empty;
+                //string controllname = string.Empty;
+                //int i = 0;
                 //获取校验失败数据字段列表，取各个字段
-                foreach (var error in validationResult.Errors)
-                {
+                //foreach (var error in validationResult.Errors)
+                //{
                     //获取错误信息
-                    message = error.ErrorMessage;
+                    //message = error.ErrorMessage;
                     //获取字段属性，视图model和界面Entry同名
-                    controllname = error.PropertyName;
+                    //controllname = error.PropertyName;
                     //取列表中第一个字段返回
-                    i++;
-                    if (i > 1)
-                        break;
-                }
+                    //i++;
+                    //if (i > 1)
+                        //break;
+                //}
                 //显示错误信息
-                DisplayAlert("提示信息", message, "确定");
+                //DisplayAlert("提示信息", message, "确定");
                 //将输入光标置于错误字段上
-                Entry entry = (Entry)FindByName(controllname);
-                entry.Focus();
+                //Entry entry = (Entry)FindByName(controllname);
+                //entry.Focus();
             }
+        }
+
+        private void DisplayError(IList<ValidationFailure> errors)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void DisplayError(List<ValidationFailure> validationFailures)
+        {
+            string message = string.Empty;
+            string controllname = string.Empty;
+            int i = 0;
+
+            foreach (var error in validationFailures)
+            {
+                //获取错误信息
+                message = error.ErrorMessage;
+                //获取字段属性，视图model和界面Entry同名
+                controllname = error.PropertyName;
+                //取列表中第一个字段返回
+                i++;
+                if (i > 1)
+                    break;
+            }
+            //显示错误信息
+            DisplayAlert("提示信息", message, "确定");
+            //将输入光标置于错误字段上
+            Entry entry = (Entry)FindByName(controllname);
+            entry.Focus();
         }
     }
 }
