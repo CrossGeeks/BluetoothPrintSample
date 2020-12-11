9bc1b643853883ad502c49db13a60b5aa32be5fc
尹庆文 <yinqingwen@gmail.com>
2020-12-11 23:54:25 +0800

    shipping ViewModel完成

--- Lims.Phone/ViewModels/ShippingViewModel.cs
+++ Lims.Phone/ViewModels/ShippingViewModel.cs
@@ -147,6 +147,242 @@ namespace Lims.Phone.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        private string _guaranteedamount;
+        /// <summary>
+        /// 保价金额
+        /// </summary>
+        public string GuaranteedAmount
+        {
+            get { return _guaranteedamount; }
+            set
+            {
+                _guaranteedamount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _collectionamount;
+        /// <summary>
+        /// 代收金额
+        /// </summary>
+        public string CollectionAmount
+        {
+            get { return _collectionamount; }
+            set
+            {
+                _collectionamount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _cashback;
+        /// <summary>
+        /// 现返金额
+        /// </summary>
+        public string Cashback
+        {
+            get { return _cashback; }
+            set
+            {
+                _cashback = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _owedback;
+        /// <summary>
+        /// 欠返金额
+        /// </summary>
+        public string OwedBack
+        {
+            get { return _owedback; }
+            set 
+            {
+                _owedback = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _deliveryfees;
+        public string DeliveryFees
+        {
+            get { return _deliveryfees; }
+            set
+            {
+                _deliveryfees = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _paymentmethods;
+        /// <summary>
+        /// 付款方式
+        /// </summary>
+        public string PaymentMethods
+        {
+            get { return _paymentmethods; }
+            set
+            {
+                _paymentmethods = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _transitmethod;
+        /// <summary>
+        /// 中转方式
+        /// </summary>
+        public string TransitMethod
+        {
+            get { return _transitmethod; }
+            set 
+            {
+                _transitmethod = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _waybillnumber;
+        /// <summary>
+        /// 单据号码
+        /// </summary>
+        public string WaybillNumber 
+        {
+            get { return _waybillnumber; }
+            set 
+            {
+                _waybillnumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _issave;
+        /// <summary>
+        /// 单据保存状态标志位。true保存完毕，flase尚未保存
+        /// </summary>
+        public bool IsSave 
+        {
+            get { return _issave; }
+            set 
+            {
+                _issave = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isprintwaybill;
+        /// <summary>
+        /// 单据打印标志位，true打印完毕,flase尚未打印
+        /// </summary>
+        public bool IsPrintWaybill 
+        {
+            get { return _isprintwaybill; }
+            set 
+            {
+                _isprintwaybill = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isprintwaybilllabels;
+        /// <summary>
+        /// 运单标签是否打印标志位，true打印完毕，flase尚未打印
+        /// </summary>
+        public bool IsPrintWaybillLabels 
+        {
+            get { return _isprintwaybilllabels; }
+            set 
+            {
+                _isprintwaybilllabels = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _islogin;
+        /// <summary>
+        /// 是否登录标记
+        /// </summary>
+        public bool IsLogin
+        {
+            get { return _islogin; }
+            set
+            {
+                _islogin = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _account;
+        /// <summary>
+        /// 账号，从login页面传递来的
+        /// </summary>
+        public string Account 
+        {
+            get { return _account; }
+            set 
+            {
+                _account = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _company;
+        /// <summary>
+        /// 公司名称，从login页面传递来的
+        /// </summary>
+        public string Company 
+        {
+            get { return _company; }
+            set 
+            {
+                _company = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _name;
+        /// <summary>
+        /// 简称
+        /// </summary>
+        public string Name 
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _date;
+        /// <summary>
+        /// 系统时间，从login页面传递过来的
+        /// </summary>
+        public string Date 
+        {
+            get { return _date; }
+            set 
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _printname;
+        /// <summary>
+        /// 默认打印机名称
+        /// </summary>
+        public string PrintName
+        {
+            get { return _printname; }
+            set 
+            {
+                _printname = value;
+                OnPropertyChanged();
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string name = "")
         {
